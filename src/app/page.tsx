9dc090727b3bcdb2678dd8ac36fa67d3b3df7835
Juan Andres <tuberquia26@hotmail.com>
2024-11-18 18:07:56 -0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+import paths from "@/path"
+
+export default async function Home() {
+
+  console.log("pathh", paths)
+
+  return (
+    <div className='flex gap-x-3 px-4 py-4'>
+
+      {/* <Link href={paths.topicToShow("test")}> Test </Link> */}
+    </div>
+
+  )
+}
